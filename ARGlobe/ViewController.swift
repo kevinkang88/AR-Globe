9375d004fa5df36ac26b98399d77d0c3c323186a
kevin <kevinkang88@gmail.com>
2018-01-27 19:14:23 -0800

    Initial Commit

---
+++ ARGlobe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ARGlobe
+//
+//  Created by kevin on 1/27/18.
+//  Copyright © 2018 yooniverse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
